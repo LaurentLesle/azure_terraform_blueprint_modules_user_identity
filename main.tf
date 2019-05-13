684945c86178424e0a72abc203a722a171b74ffc
Laurent <laurent.lesle@microsoft.com>
2019-05-13 19:37:20 +0800

    Let the prefix including or not the hyphen

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ resource "azurerm_user_assigned_identity" "msi" {
     resource_group_name     = "${var.resource_group_name}"
     location                = "${var.location}"
 
-    name = "${var.prefix}-${var.name}"
+    name = "${var.prefix}${var.name}"
 }
